2f3a9944c433f5e138bc350e4864aa0a43fd8ee9
phonelykangs <68152594+phonelykangs@users.noreply.github.com>
2020-07-13 08:42:31 +0800

    Revert "Update views.py"
    This reverts commit 32d7bcf267c76f252f2f64281ec790e79f858a81.

--- chart_demo/dbdata/views.py
+++ chart_demo/dbdata/views.py
@@ -1,23 +1,3 @@
 from django.shortcuts import render
-from .models import *
-import collections
 
 # Create your views here.
-
-def get_Com_numbrt():       #返回每个公司及申请数量：按数量排序
-    list_all = ChartInfo.objects.all()
-    list_comp_all = []
-    for i in list_all:
-        list_comp_all.append((i.PA))
-    Counter_comp = collections.Counter(list_comp_all)
-    dict_comp = dict(Counter_comp)
-    list_1 = sorted(dict_comp.items(), key=lambda x: x[1], reverse=True)
-    return list_1
-def chart(request):            #返回数量排名前8的公司及数量
-    list_comp=[]
-    list_comp_freq=[]
-    list_1=get_Com_numbrt()
-    for i in range(0,8):
-        list_comp.append(list_1[i][0])
-        list_comp_freq.append(list_1[i][1])
-    return render(request,'chart.html',{'list_comp':list_comp,'list_comp_freq':list_comp_freq})
